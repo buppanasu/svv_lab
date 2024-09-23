0853060d1b1f5b5e978febdbc347e5fbd64263f9
buppanasu <2201196sit@gmail.com>
2024-09-23 17:25:40 +0800

    test change

--- ICT3101_Calculator/Program.cs
+++ ICT3101_Calculator/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //hello
+        //hello Test Change
         bool endApp = false;
         Calculator _calculator = new Calculator();
         // Display title as the C# console calculator app.
